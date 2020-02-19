338a5e746cc843b3bac4017176f0e7e7e0133a2a
Adel Chabli <adel.chabli@hotmail.fr>
2020-02-19 09:58:45 +0100

    Vérification permission, ajout sweetAlert, ajout directoryManager

--- app/src/main/java/com/example/voicy_v2/activity/MainActivity.java
+++ app/src/main/java/com/example/voicy_v2/activity/MainActivity.java
@@ -2,16 +2,20 @@ package com.example.voicy_v2.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import com.example.voicy_v2.R;
 import com.example.voicy_v2.interfaces.CallbackServer;
+import com.example.voicy_v2.model.DirectoryManager;
 import com.example.voicy_v2.model.RequestPhoneme;
 import com.example.voicy_v2.model.RequestPhrase;
 import com.example.voicy_v2.model.ServerRequest;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
 
 public class MainActivity extends AppCompatActivity implements CallbackServer
 {
@@ -22,6 +26,8 @@ public class MainActivity extends AppCompatActivity implements CallbackServer
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DirectoryManager.getInstance().initProject();
 
         // Déclaration
         requestPhoneme = new RequestPhoneme(this, MainActivity.this);
@@ -49,4 +55,28 @@ public class MainActivity extends AppCompatActivity implements CallbackServer
     public void traitementPhoneme(String response) {}
 
     public void traitementPhrase(String response) {}
+
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(false);
+
+        new cn.pedant.SweetAlert.SweetAlertDialog(this, cn.pedant.SweetAlert.SweetAlertDialog.WARNING_TYPE)
+                .setTitleText("Êtes-vous sûr ?")
+                .setContentText("Vous aller quitter l'application.")
+                .setConfirmText("Quitter")
+                .setConfirmClickListener(new cn.pedant.SweetAlert.SweetAlertDialog.OnSweetClickListener() {
+                    @Override
+                    public void onClick(cn.pedant.SweetAlert.SweetAlertDialog sDialog)
+                    {
+                        finishAffinity();
+                    }
+                })
+                .setCancelButton("Annuler", new cn.pedant.SweetAlert.SweetAlertDialog.OnSweetClickListener() {
+                    @Override
+                    public void onClick(cn.pedant.SweetAlert.SweetAlertDialog sDialog) {
+                        sDialog.dismissWithAnimation();
+                    }
+                })
+                .show();
+    }
 }
---
+++ app/src/main/java/com/example/voicy_v2/model/DirectoryManager.java
@@ -0,0 +1,80 @@
+package com.example.voicy_v2.model;
+
+import android.os.Environment;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class DirectoryManager
+{
+
+    // -------------------- SINGLETON ---------------------------
+    private DirectoryManager() {}
+
+    private static DirectoryManager INSTANCE = null;
+
+    public static synchronized DirectoryManager getInstance()
+    {
+        if (INSTANCE == null) { INSTANCE = new DirectoryManager(); }
+        return INSTANCE;
+    }
+    // -----------------------------------------------------------
+
+    // Arguments
+    public static final String OUTPUT_DIRECTORY = Environment.getExternalStorageDirectory() + "/Voicy";
+
+    public void initProject()
+    {
+        createInternalDirectory();
+        createFolderInAppFolder("Phonemes");
+        createFolderInAppFolder("Phrases");
+    }
+
+    // Créer le dossier de notre application sur l'appareil Android
+    private void createInternalDirectory()
+    {
+        File file = new File(OUTPUT_DIRECTORY);
+        if (!file.exists())
+            file.mkdir();
+    }
+
+    // Créer un dossier à l'intérieur du dossier de l'application
+    public void createFolderInAppFolder(String directoryName)
+    {
+        File file = new File(OUTPUT_DIRECTORY + "/" + directoryName);
+        if (!file.exists())
+            file.mkdir();
+    }
+
+    // Permet de supprimer un dossier et son contenu dans le dossier de l'application
+    public void rmdirFolderInAppFolder(String directoryName)
+    {
+        File file = new File(OUTPUT_DIRECTORY + "/" + directoryName);
+        if (file.isDirectory())
+        {
+            String[] children = file.list();
+            for (int i = 0; i < children.length; i++)
+            {
+                new File(file, children[i]).delete();
+            }
+        }
+    }
+
+    // Permet de supprimer tous les dossiers de l'application
+    public void rmdirAllAppFolder()
+    {
+        File file = new File(OUTPUT_DIRECTORY);
+        if (file.isDirectory())
+        {
+            String[] children = file.list();
+            for (int i = 0; i < children.length; i++)
+            {
+                if(file.isDirectory())
+                    rmdirFolderInAppFolder(file.getName());
+                else
+                    file.delete();
+            }
+        }
+    }
+}
